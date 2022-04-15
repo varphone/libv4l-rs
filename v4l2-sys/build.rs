1075563ede3d3941f05aec24c0511158b9f409de
Varphone Wong <varphone@qq.com>
2022-04-15 14:30:21 +0800

    Add TARGET_SYSROOT_DIR supports
    Signed-off-by: Varphone Wong <varphone@qq.com>

--- v4l2-sys/build.rs
+++ v4l2-sys/build.rs
@@ -1,11 +1,17 @@
-extern crate bindgen;
-
 use std::env;
 use std::path::PathBuf;
 
+const DEFAULT_TARGET_SYSROOT_DIR: &str = "/opt/fullv/2021.02.8-rklaser1/staging";
+
 fn main() {
+    println!("cargo:rerun-if-env-changed=TARGET_SYSROOT_DIR");
+
+    let target_sysroot_dir =
+        env::var("TARGET_SYSROOT_DIR").unwrap_or_else(|_| DEFAULT_TARGET_SYSROOT_DIR.into());
+
     let bindings = bindgen::Builder::default()
         .header("wrapper.h")
+        .clang_arg(format!("--sysroot={}", target_sysroot_dir))
         .generate()
         .expect("Failed to generate bindings");
 
